4ee93458c738a86705598821d2a84d2608ee9ed2
xxxsin <162655489+xxxsin@users.noreply.github.com>
2024-03-11 19:10:11 +0900

    Title: 두수의 곱과 몫, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240311/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
+++ 240311/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
@@ -4,7 +4,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
-        System.out.println("300 * 5 = "+a*b);
-        System.out.println("300 / 5 = "+a/b);
+        System.out.printf("%d * %d = %d\n",a,b,(a*b));
+        System.out.printf("%d / %d = %d\n",a,b,(a/b));
     }
 }
