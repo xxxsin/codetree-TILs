f84a1ca77e1d0400d7f55d0de5929c828e8ee8e4
xxxsin <162655489+xxxsin@users.noreply.github.com>
2024-03-11 13:08:52 +0900

    Title: 합을 복사하기, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240311/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=1,b=2,c=3;
+        int d=a+b+c;
+        a=b=c=d;
+
+        System.out.println(a+" "+b+" "+c);
+    }
+}
