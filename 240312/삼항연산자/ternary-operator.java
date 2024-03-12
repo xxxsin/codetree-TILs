60b6850dc41c14a8d0938c8d673b75ba1600fd84
xxxsin <162655489+xxxsin@users.noreply.github.com>
2024-03-12 11:00:40 +0900

    Title: 삼항연산자, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240312/삼항연산자/ternary-operator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        String a = n==100 ? "pass":"failure";
+
+        System.out.println(a);
+         }
+}
