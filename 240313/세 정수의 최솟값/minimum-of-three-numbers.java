2184777b6c70db8a9caf5a6c1c2b236ea307b194
xxxsin <162655489+xxxsin@users.noreply.github.com>
2024-03-13 13:42:01 +0900

    Title: 세 정수의 최솟값, Time: 192ms, Memory: 10MB, Status: Passed - CodeTree

--- 240313/세 정수의 최솟값/minimum-of-three-numbers.java
+++ 240313/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -6,13 +6,13 @@ public class Main {
             int b=sc.nextInt();
             int c=sc.nextInt();
 
-            if (a<b && a<c){
+            if (a<=b && a<=c){
                 System.out.println(a);
             }
-            else if (b<a && b<c){
+            else if (b<=a && b<=c){
                 System.out.println(b);
             }
-            else if (c<a && c<b){
+            else if (c<=a && c<=b){
                 System.out.println(c);
             }
             else{
