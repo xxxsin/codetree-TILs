fe88edd795c013290164db9742d2d66b34ddd563
xxxsin <162655489+xxxsin@users.noreply.github.com>
2024-03-14 17:07:05 +0900

    Title: 좀 더 어려운 수학 점수, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240314/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
+++ 240314/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -3,11 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int aeng = sc.nextInt();
         int amath = sc.nextInt();
+        int aeng = sc.nextInt();
 
-        int beng = sc.nextInt();
         int bmath = sc.nextInt();
+        int beng = sc.nextInt();
 
         if (amath > bmath || (amath == bmath && aeng > beng)){
             System.out.println("A");
