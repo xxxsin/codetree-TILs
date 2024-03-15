3720a8db4b4b8b018aeb0238ebc4d4852316dae4
xxxsin <162655489+xxxsin@users.noreply.github.com>
2024-03-15 15:54:07 +0900

    Title: 윤년인가, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240315/윤년인가/is-leap-year.java
+++ 240315/윤년인가/is-leap-year.java
@@ -7,10 +7,10 @@ public class Main {
         if (y%4==0){
                 System.out.println("true");
             }
-        else if (y%100==0 || y%400==0){
-            System.out.println("ture");
+        else if (y%100==0 || y%400!=0){
+            System.out.println("flase");
             }
-        else if(y%400!=0){
+        else {
             System.out.println("false");
         }
     }
