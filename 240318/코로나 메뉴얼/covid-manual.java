dc7407311e0740ea8d5f1c73ad46b389741bb3cc
xxxsin <162655489+xxxsin@users.noreply.github.com>
2024-03-18 14:13:51 +0900

    Title: 코로나 메뉴얼, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240318/코로나 메뉴얼/covid-manual.java
+++ 240318/코로나 메뉴얼/covid-manual.java
@@ -1,51 +1,36 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        char c1, c2, c3;
+        int t1, t2, t3;
         
-        char acorona = sc.next().charAt(0);
-        int atemp = sc.nextInt();
+        // 입력
+        c1 = sc.next().charAt(0);
+        t1 = sc.nextInt();
+        c2 = sc.next().charAt(0);
+        t2 = sc.nextInt();
+        c3 = sc.next().charAt(0);
+        t3 = sc.nextInt();
 
-        char bcorona = sc.next().charAt(0);
-        int btemp = sc.nextInt();
-
-        char ccorona = sc.next().charAt(0);
-        int ctemp = sc.nextInt();
-
-        if (acorona == 'Y' && bcorona == 'Y' ){
-            if((atemp >= 37)&&(btemp >= 37)){
+        // 출력
+        // A가 2명 이상인지 판단하기
+        if(c1 == 'Y' && t1 >= 37) {
+            // 첫 번째 사람이 A라면, 남은 두 사람 중 한 사람이라도 A면 됩니다.
+            if((c2 == 'Y' && t2 >= 37) || (c3 == 'Y' && t3 >= 37))
                 System.out.println("E");
-            }
-            else{
+            else
                 System.out.println("N");
-            }
         }
-        else if (bcorona == 'Y'&&ccorona == 'Y'){
-            if((btemp >= 37)&&(ctemp >= 37)){
+        else {
+            // 첫 번째 사람이 A가 아니라면, 남은 두 사람 모두 A여야만 합니다.
+            if((c2 == 'Y' && t2 >= 37) && (c3 == 'Y' && t3 >= 37))
                 System.out.println("E");
-            }
-            else{
+            else
                 System.out.println("N");
-            }
-        }
-        else if(ccorona == 'Y'&& acorona == 'Y'){
-            if((ctemp >=37)&& (atemp >= 37)){
-                System.out.println("E");
-            }
-            else{
-                System.out.println("N");
-            }
-        }
-        else if (acorona == 'Y' && bcorona == 'Y' && ccorona == 'Y'){
-            if (atemp >= 37||btemp >= 37||ctemp >= 37){
-                System.out.println("E");
-            }
-            else{
-                System.out.println("N");
-            }
-        }
-        else{
-            System.out.println("N");
         }
     }
 }
