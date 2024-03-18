2e9ec619dd1767030630787234865495d474bfb3
xxxsin <162655489+xxxsin@users.noreply.github.com>
2024-03-18 13:25:33 +0900

    Title: 코로나 메뉴얼, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240318/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        char acorona = sc.next().charAt(0);
+        int atemp = sc.nextInt();
+
+        char bcorona = sc.next().charAt(0);
+        int btemp = sc.nextInt();
+
+        char ccorona = sc.next().charAt(0);
+        int ctemp = sc.nextInt();
+
+        if (acorona == 'Y' && bcorona == 'Y' ){
+            if((atemp >= 37)&&(btemp >= 37)){
+                System.out.println("E");
+            }
+            else{
+                System.out.println("N");
+            }
+        }
+        else if (bcorona == 'Y'&&ccorona == 'Y'){
+            if((btemp >= 37)&&(ctemp >= 37)){
+                System.out.println("E");
+            }
+            else{
+                System.out.println("N");
+            }
+        }
+        else if(ccorona == 'Y'&& acorona == 'Y'){
+            if((ctemp >=37)&& (atemp >= 37)){
+                System.out.println("E");
+            }
+            else{
+                System.out.println("N");
+            }
+        }
+        else{
+            System.out.println("N");
+        }
+    }
+}
