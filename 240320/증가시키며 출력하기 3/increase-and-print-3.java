bf1ace9c340f494a5b9f876de90fa00510b3844b
xxxsin <162655489+xxxsin@users.noreply.github.com>
2024-03-20 15:38:25 +0900

    Title: 증가시키며 출력하기 3, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240320/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i=10;
+
+        while(i<=26){
+            System.out.print(i+" ");
+            i+=2;
+        }
+    }
+}
