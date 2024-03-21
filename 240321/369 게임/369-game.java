841af72b08771c43447233fd95f0c244d56ace2d
xxxsin <162655489+xxxsin@users.noreply.github.com>
2024-03-21 16:10:36 +0900

    Title: 369 게임, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240321/369 게임/369-game.java
+++ 240321/369 게임/369-game.java
@@ -5,7 +5,7 @@ public class Main {
         int n = sc.nextInt();
 
         for(int i=1; i<=n; i++){
-            if(i%3==0||i==3||i==6||i==9){
+            if(i%3==0||i%10==3||i%10==6||i%10==9){
                 System.out.print(0+" ");
             }
             else{
