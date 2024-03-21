55deba3688b6d5d055f1727938ef38b1223e16dc
xxxsin <162655489+xxxsin@users.noreply.github.com>
2024-03-21 11:34:26 +0900

    Title: n번 더하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240321/n번 더하기/add-n-times.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            System.out.println(a+n);
+            a=a+n;
+        }
+    }
+}
