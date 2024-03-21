4dfd55ffc198120710dea2209cb8cc95ca9b9bec
xxxsin <162655489+xxxsin@users.noreply.github.com>
2024-03-21 16:39:37 +0900

    Title: 369 게임, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240321/369 게임/369-game.java
+++ 240321/369 게임/369-game.java
@@ -3,14 +3,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        for(int i=1; i<=n; i++){
-            if(i%3==0||i%10==3||i%10==6||i%10==9){
-                System.out.print(0+" ");
+            for(int i=1; i<=n; i++){
+                if((i%3==0) || i/10==3 || i/10==6 || i/10==9){
+                    System.out.print(0+" ");
+                }
+                else{
+                    System.out.print(i+" ");
             }
-            else{
-                System.out.print(i+" ");
-            }
-        }
     }
+}
 }
