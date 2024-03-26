f2e9c001154155f718d771d1fae315b77931e00a
xxxsin <162655489+xxxsin@users.noreply.github.com>
2024-03-26 16:49:34 +0900

    Title: 배수의 개수 2, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240326/배수의 개수 2/number-of-multipliers-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for(int i=1; i<=10; i++){
+            if (i%2==0){
+                cnt++;
+            } 
+        }System.out.println(cnt);
+
+    }
+}
